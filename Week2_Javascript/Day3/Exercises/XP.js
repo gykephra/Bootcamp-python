5bf96a82063b4f828f886de479de7afa3081b046
gykephra <yaoephra90@gmail.com>
2022-09-29 14:32:43 +0000

    today commit

--- Week2_Javascript/Day3/Exercises/XP.js
+++ Week2_Javascript/Day3/Exercises/XP.js
@@ -1,2 +1,36 @@
 /*Exercise1*/
 let people = ["Greg", "Mary", "Devon", "James"];
+people.shift();
+people.splice(3, 1, "Jason");
+people.push("Ephraîm");
+console.log(indexOf("Mary"))
+console.log(people.slice(1,3))
+people.indexOf("Foo")//-1 because Foo doesn't in people
+let last = people[people.length -1]
+
+/*Partie II*/
+for (let name=0; name<people.length; name++) {
+	console.log(people[name])
+}
+
+for (let name=0; name<people.length; name++) {
+	if(name==="Jason"){
+		break;
+	}
+	console.log(people[name])
+}
+
+
+/*Exercise2*/
+let colors = ["Blue", "White", "Black", "Red", "Green"]
+for (let color=0; color<colors.length; color++) {
+	console.log("My "+colors.indexOf(color)+" choice is ", colors[color])
+}
+
+let colors = ["Blue", "White", "Black", "Red", "Green"]
+//let choices = ["1st", "2nd", "3rd", "4th", "5th"]
+for (let color=0; color<colors.length; color++)
+//for (let choice = 0, choice<choices.lenth; choice++)
+{
+	console.log("My "+choices[choice]+" choice is ", colors[color])
+}
---
+++ Week4_Python/Day1/XP.py
@@ -0,0 +1,62 @@
+#Exercise1
+H="Hello world"
+print(H+"\n",H+"\n",H+"\n",H+"\n")
+
+#Exercise2
+99**3*8
+
+#Exercise3
+True
+True
+False
+False
+Error
+
+#Exercise4
+computer_brand="hp"
+print("I have a "+computer_brand+" computer")
+
+#Exercise5
+name = "Ephraim"
+age = "23"
+shoe_size = "45"
+info="My name is " +name +" and i am "+age+ " yo. i wear size "+shoe_size+" , yes i have big feet lol"
+print(info)
+#OR
+info = f"My name is {name} and i am {age} yo. I wear {shoe_size}, yes i have big feet lol"
+print(info)
+
+
+#Exercise6
+a = 15
+b = 10
+if a > b:
+	print("Hello World")
+
+
+#Exercise7
+numb= int(input("Type a number : "))
+if numb%2 == 0:
+	print(numb, " is odd")
+else:
+	print(numb," is even")
+
+
+#Exercise8
+My_name= input("Type your name : ")
+if My_name=="Ephraïm":
+	print("Hello",My_name,"we have a same name")
+else:
+	print("I expected we have same name, but nice name anyway")
+
+#Exercise9
+your_height = float(input("Type your height in inch please : "))
+if your_height*2.54 >145:
+	print("you are tall enough to ride roller coaster")
+else:
+	print("sorry you need to grow more to ride")
+
+
+
+
+
