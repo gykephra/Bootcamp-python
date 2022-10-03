5c141d5dc989da2dee67c0a56f90e19a77352d61
gykephra <yaoephra90@gmail.com>
2022-10-03 14:39:56 +0000

    commit

---
+++ Week6_Databases/Day1/challenge.sql
@@ -0,0 +1,18 @@
+-- Database: Hollywood
+
+-- DROP DATABASE IF EXISTS "Hollywood";
+
+-- CREATE DATABASE "Hollywood"
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'French_France.1252'
+--     LC_CTYPE = 'French_France.1252'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
+
+SELECT count(*) FROM actors;
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Tyler','','', 2); -- error
